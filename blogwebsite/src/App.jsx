109b528ca584f3f10b432c4a53f2a9d5dbd37c6e
Haridas Pawar <haridaspawar0203@gmail.com>
2024-03-31 20:25:10 +0530

    Blog React App Initial Commit

---
+++ blogwebsite/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  // It is for the app that is created using -> create react app
+  // console.log(process.env.REACT_APP_APPWRITE_URL)
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+    <h1>Blog App with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
